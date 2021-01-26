fba2949b537e989b74fff1c0b24abdda9f1782b0
Pri-Alves <priscillarafaela19@gmail.com>
2021-01-25 23:37:53 -0300

    adicionando exercicio-data-binding

---
+++ src/app/exercicio-data-binding/exercicio-data-binding.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exercicio-data-binding',
+  templateUrl: './exercicio-data-binding.component.html',
+  styleUrls: ['./exercicio-data-binding.component.scss']
+})
+export class ExercicioDataBindingComponent implements OnInit {
+
+  imageUrl = "https://image.winudf.com/v2/image1/Y29tLnRhbWFyYWFwcHMuY3V0ZWxpdHRsZWtpdHRlbnNfc2NyZWVuXzRfMTU1ODgxNDI0Nl8wNDc/screen-4.jpg?fakeurl=1&type=.jpg";
+  initialValue = 'valor inicial'
+  isDisabled = true;
+  accessibiityText = 'Botão Habilitado';
+  
+  constructor() {
+    setTimeout(() => {
+      this.isDisabled = false;
+      console.log('isDisabled: ', this.isDisabled);
+    }, 3000);
+   }
+
+  ngOnInit(): void {
+  }
+
+
+}

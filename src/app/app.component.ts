7367ec91aeffa0f2df6ff223c6987493ce9086b1
Pri-Alves <priscillarafaela19@gmail.com>
2021-01-27 19:41:26 -0300

    Testando Life Cycle Hooks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,17 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
 
+  showHeader = true;
   palavra = 'palavra';
   corfavorita = 'verde';
   titulo = 'Seja bem vindo!';
 
   constructor(){  }
-
+  
+  ngOnInit() {
+    setTimeout(() => {
+      this.titulo = 'outra coisa';
+    
+      setTimeout(() => {
+        this.showHeader = false;
+      }, 3000)
+    
+    }, 3000)
+  }
+  
   eventoRecebido($event:any){
     console.log('appComponente: evento recebido!', $event);
   }
-  
 
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,18 +1,32 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnChanges, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnChanges, OnInit,  AfterViewInit, OnDestroy {
   @Input() titulo: string
 
   constructor() { 
+    console.log('Construtor');
     this.titulo='';
   }
 
+  ngOnChanges(): void {
+    console.log('ngOnChanges');
+  }
+  
   ngOnInit(): void {
+    console.log('ngOnInit');
+  }
+
+  ngAfterViewInit(){
+    console.log('ngAfterViewInit');
+  }
+
+  ngOnDestroy(){
+    console.log('ngOnDestroy');
   }
 
 }

143d5953fcbb320272fda232b622f59b2c824868
Pri-Alves <priscillarafaela19@gmail.com>
2021-01-27 23:26:20 -0300

    Criando Rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,9 @@ import { ExercicioNgclassComponent } from './shared/components/exercicio-ngclass
 import { ExercicioPipesComponent } from './shared/components/exercicio-pipes/exercicio-pipes.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { HeaderComponent } from './shared/components/header/header.component';
+import { LoginComponent } from './login/login.component';
+import { ExtratoComponent } from './extrato/extrato.component';
+import { NaoEncontradoComponent } from './nao-encontrado/nao-encontrado.component';
 
 registerLocaleData(localePt, 'pt')
 @NgModule({
@@ -25,7 +28,10 @@ registerLocaleData(localePt, 'pt')
     ExercicioContadorComponent,
     ExercicioDiretivasComponent,
     ExercicioNgclassComponent,
-    ExercicioPipesComponent
+    ExercicioPipesComponent,
+    LoginComponent,
+    ExtratoComponent,
+    NaoEncontradoComponent
   ],
   imports: [
     BrowserModule,
